02d1e1dcf8bf567dc76a9ac079ac3aad40d3ac92
watermouth <at.imac>
2014-02-05 07:54:34 +0900

    p.73までを追加

--- R/s2.R
+++ R/s2.R
@@ -56,7 +56,6 @@ legend("bottomleft", col=c("darkgrey", rep("black", 2))
        , legend=c("data", "smoothed level", "95% probability lines"))
 
 # p.67
-
 # expenditure
 expd <- ts(read.table("inst//extdata/qconsum.dat",skip=4
                       , colClasses="numeric")[,1]
@@ -81,12 +80,71 @@ legend("bottomright", col=c("darkgrey", rep("black", 2))
        , pch = c(1,NA,NA)
        , bty = "n"
        , legend=c("data", "filtered level", "smoothed level"))
+
+### 寄り道: parameter estimation with using most likelyhood
+# expdFiltによる予測
+lines(expdFilt$f, lty="dotdash", col="green")
+# ARMA付加(AR(3))
+build <- function(param){
+  expd.dlm + dlmModARMA(ar=c(0,param[1],param[2]), sigma2=exp(param[3]), dV=exp(param[4]))
+}
+fit <- dlmMLE(y=expd, parm=rep(0,4), build=build)
+expdFiltWithARMA <- dlmFilter(y=expd, mod=build(param=fit$par))
+lines(dropFirst(x=expdFiltWithARMA$m[,1]), lty="dashed", col="red")
+# 予測 の つもり: expdFiltと一致してしまうように見える.かなり近いが,ARをつけたためか引きずられ気味になる.
+# もともとのexpdFiltを前提にしているからだろう.
+# expdFilt自体のパラメータ推定を含めて試行しておきたい.
+lines(x=expdFiltWithARMA$f, lty="dashed", col="blue")
+
 ### Seasonal component
 plot(dropFirst(expdSmooth$s[,2]), type="o", xlab = "", ylab="Expenditure - Seasonal component")
 abline(h=0)
 
-### parameter estimation with using most likelyhood
-build <- function(param){
-  expd.dlm + dlmModARMA(ar=2, ma=2, sigma2=exp(param[1]), dV=exp(param[2]))
-}
-fit <- dlmMLE(y=expd, parm=rep(0,2), build=build)
+# p.68 prediction
+# Nile
+a <- window(x=cbind(Nile, NileFilt1$f, NileFilt2$f)
+            , start= 1880, end=1920)
+plot(a[,1], type="o", col="darkgrey", xlab="", ylab="Level")
+lines(a[,2], lty="longdash")
+lines(a[,3], lty="dotdash")
+leg <- c("data", paste("one-step-ahead forecast, W/V = "
+                       , format(c(W(mod1) / V(mod1),
+                                  W(mod2) / V(mod2)))))
+legend("bottomleft", legend=leg, col=c("darkgrey", rep("black",2))
+       , lty=c("solid", "longdash", "dotdash")
+       , pch=c(1,NA,NA)
+       , bty="n")
+
+# p.69 : time-variant variance
+mod0 <- dlmModPoly(order=1, dV=15100, dW=1468)
+X <- ts(matrix(mod0$W, ncol=1, nrow=length(Nile)), start=start(Nile))
+window(X, 1898, 1899) <- 12 * mod0$W
+modDam <- mod0
+modDam$X <- X
+modDam$JW <- matrix(data=1, nrow=1,ncol=1)
+damFilt <- dlmFilter(y=Nile, mod=modDam)
+mod0Filt <- dlmFilter(y=Nile, mod=mod0)
+# most of all codes following this line are copied and pasted.
+a <- window(x=cbind(Nile, mod0Filt$f, damFilt$f)
+            , start= 1880, end=1920)
+plot(a[,1], type="o", col="darkgrey", xlab="", ylab="Level")
+lines(a[,2], lty="longdash")
+lines(a[,3], lty="dotdash")
+abline(v=1898, lty=2)
+leg <- c("data", paste("one-step-ahead forecast, W/V = "
+                       , c("mod0", "modDam")))
+legend("bottomleft", legend=leg, col=c("darkgrey", rep("black",2))
+       , lty=c("solid", "longdash", "dotdash")
+       , pch=c(1,NA,NA)
+       , bty="n")
+
+# p.73 : k-step ahead forecasting simulation
+set.seed(1)
+expdFore <- dlmForecast(mod=expdFilt, nAhead=12, sampleNew=100)
+plot(window(x=expd, start=c(1964,1)), type="o"
+     , xlim=c(1964, 1971), ylim=c(350,850)
+     , xlab="", ylab="Expenditures")
+names(expdFore)
+invisible(lapply(expdFore$newObs, function(x) lines(x, col = "darkgrey", type='o', pch=4)))
+lines(expdFore$f, type='o', lwd=2, pch=16)
+abline(v=mean(c(time(expdFore$f)[1], time(expd)[length(expd)])), lty="dashed")
