3202af47d4f0422f11a9cdf6ccda59e785ec1b57
watermouth <at.imac>
2014-02-08 15:55:29 +0900

    p.95, 96, dlmModPoly & the lake of Superior

---
+++ R/s3.R
@@ -0,0 +1,30 @@
+s3 <- function(polydim=1){
+  # p.95, 96
+  lakeSup <- ts(read.table("inst//extdata/lakeSuperior.dat",skip=3
+                           , colClasses="numeric")[,2]
+                , start=1900)
+  build <- function(para){
+    dlmModPoly(order=polydim, dV=exp(para[1]), dW=exp(para[2:(polydim+1)]), m0=rep(lakeSup[1], polydim), C0=1e7 * diag(polydim))
+  }
+  mle.result <- dlmMLE(y=lakeSup, parm=rep(0, (polydim+1)), build=build)
+  modLSup <- build(mle.result$par)
+  str(modLSup,1)
+  lSupFilt <- dlmFilter(y=lakeSup, mod=modLSup)
+  res <- residuals(object=lSupFilt, sd=F)
+  plot(lakeSup, type="o", col="darkgrey")
+  lines(lSupFilt$f, col="red")
+  shapiro.test(res)
+  Box.test(x=res, lag=20, type="Ljung")
+  sapply(X=1:20, function(i)
+    Box.test(res, lag=i, type="Ljung-Box")$p.value
+  )
+  tsdiag(lSupFilt)
+  
+  # p.96 HoltWinters function
+  HWout <- HoltWinters(x=lakeSup, gamma=F, beta=F)
+  plot(lakeSup, type="o", col="darkgrey")
+  lines(dropFirst(lSupFilt$f), lty="dashed", xlab="", ylab="")
+  lines(HWout$fitted[, "level"])
+  leg <- c("Holt-Winters", "Local level DLM")
+  legend("topleft", legend = leg, bty="n", lty=c("solid", "dashed"))
+}
