d9e3cbf7a8f235d7e60ff38960004931c851a5dd
watermouth <at.imac>
2014-02-08 10:36:08 +0900

    演習問題2.5つづき

--- R/e2.R
+++ R/e2.R
@@ -16,6 +16,7 @@ e2_5 <- function(){
   # 理想的な予測であれば、グレーラインを左にずらしたような線になる。
   
   # (c)
+  print("(c)")
   pairs <- combn(x=c(0.01, 0.25, 1, 5, 25, 125), m=2)
   invisible(
     sapply(X=1:ncol(pairs), FUN=function(i){
@@ -35,4 +36,28 @@ e2_5 <- function(){
       )
     })
   )
+  # W/V比が大きいほど, 実績データに当てはまる.
+  # (d)
+  print("(d)")
+  # 1点目のデータに近い点から始めることで,初期の 当てはまりを良くできる.
+  mod.ll <- dlmModPoly(order=1, dV=0.25, dW=25, m0=1, C0=0.3)
+  mod.ll.filt <- dlmFilter(y=y, mod=mod.ll)
+  mod.ll2 <- dlmModPoly(order=1, dV=0.25, dW=25, m0=1, C0=30)
+  mod.ll2.filt <- dlmFilter(y=y, mod=mod.ll2)
+  # plot 
+  plot(y, col="grey", pch=20, type="o")
+  lines(dropFirst(mod.ll.filt$m), col="blue", lwd=0.5)
+  lines(mod.ll.filt$f, pch=20, type="l", lwd=0.5)
+  lines(dropFirst(mod.ll2.filt$m), col="blue", lwd=0.5, lty="longdash")
+  lines(mod.ll2.filt$f, pch=20, type="l", lwd=0.5, lty="longdash")
+  
+  # (e)
+  print("(e)")
+  m0(mod.ll) <- 17
+  C0(mod.ll) <- 1
+#  smooth.ll <- dlmSmooth(mod.ll.filt)
+  smooth.ll <- dlmSmooth(y=y, mod=mod.ll)
+  plot(y, col="darkgrey", pch=20, type="o")
+#  lines(dropFirst(mod.ll.filt$m), col="blue", lwd=0.5)
+  lines(dropFirst(smooth.ll$s), col="black", lwd=0.5, lty="dotdash")
 }
