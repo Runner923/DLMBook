444a8e5c549fde7aa2e61fded31e9b167a7afae7
watermouth <sminakuchi@gmail.com>
2014-04-06 10:24:32 +0900

    ggplot2でプロットするのに便利な変換関数 TimeSeries -> DataFrame, DataFrame -> DataFrame, DLMFilter obj -> DataFrame

---
+++ R/utility.R
@@ -0,0 +1,44 @@
+#' \code{TStoDF}
+#' @description TimeSeries オブジェクトをDataFrameに変換
+#' for plotting with ggplot2
+TStoDF <- function(obj.ts, target.columns=colnames(obj.ts), output.columns=(target.columns)){
+  # tがtime-seriesだとmeltでまずいことになる
+  temp.dt <- data.frame(t=as.numeric(time(obj.ts)), obj.ts)
+  names(temp.dt)[-1] <- output.columns
+  dt <- melt(data=temp.dt, id.vars="t", measure.vars=output.columns)
+  dt
+}
+
+#' \code{TimeSeriesDFtoDFforggplot}
+#' @description 素朴な時系列データフレーム
+#' 
+TimeSeriesDFtoDFforggplot <- function(dt, id="t", target.columns=colnames(dt)){
+  dt <- melt(data=dt, id.vars=id, measure.vars=target.columns[target.columns != id])
+  dt
+}
+
+DLMFilteredPredictionToDF <- function(dlmFiltered, exclude.indices=NULL, columnIndex=1, conf.interval=0.5){
+  i <- columnIndex
+  ts.y <- dlmFiltered$y[,i]
+  original <- as.numeric(ts.y)
+  t <- as.numeric(time(ts.y))
+  mean.pred <-as.numeric(dlmFiltered$f[,i])
+  dt <- data.frame(t=t[-1], mean.pred = mean.pred[-length(mean.pred)], original=original[-1])
+  if(is.numeric(conf.interval) && abs(conf.interval) <= 1){
+    sdev <- residuals(object=dlmFiltered)$sd
+    interval.width <- qnorm(0.5 + 0.5 * conf.interval) * sdev
+    upr <- dlmFiltered$f + interval.width 
+    lwr <- dlmFiltered$f - interval.width 
+    upr <- as.numeric(upr[,i])
+    lwr <- as.numeric(lwr[,i])
+    dt$upr = upr[-length(upr)]
+    dt$lwr = lwr[-length(lwr)]
+  }
+  if(is.integer(exclude.indices)){
+    dt <- dt[-exclude.indices,]
+  }
+  dt <- TimeSeriesDFtoDFforggplot(dt=dt)
+  dt$colour <- as.character(dt$variable)
+  dt$colour[dt$colour == "lwr" | dt$colour == "upr"] <- "conf.bound"
+  dt
+}
